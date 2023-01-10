1b90ac758714593645f3fd441104987af48414a7
田子友唯 <yui@tagotomotadanoMacBook-Pro.local>
2023-01-10 16:06:22 +0900

    データ成形したよ

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
+const node = [];
+const nodeIDs = [];
+const link = [];
+
+chrome.history.onVisited.addListener((re) => {
+  if (!nodeIDs.includes(re.id)) {
+    node.push({ data: { id: re.id, url: re.url } });
+    nodeIDs.push(re.id);
+  }
+
+  if (nodeIDs.length !== 1) {
+    link.push({
+      data: { target: re.id, source: nodeIDs[nodeIDs.length - 2] },
+    });
+  }
+  console.log(node);
+  console.log(link);
+});
+
 const App = () => {
   return (
     <div>
