6acd9c8249de302fa57b6d04b91f1a9166f4b847
itochs <halglobe0108@gmail.com>
2023-01-11 21:44:02 +0900

    nodeの追加を関数内に変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HistoryChart from "./HistoryChart";
-import data from "./_dammy.json";
+// import data from "./_dammy.json";
 
-const node = [];
-const nodeIDs = [];
-const link = [];
-
-chrome.history.onVisited.addListener((re) => {
-  if (!nodeIDs.includes(re.id)) {
-    node.push({ data: { id: re.id, url: re.url } });
-    nodeIDs.push(re.id);
-  }
-
-  if (nodeIDs.length !== 1) {
-    link.push({
-      data: { target: re.id, source: nodeIDs[nodeIDs.length - 2] },
-    });
-  }
-  console.log(node);
-  console.log(link);
-});
+// const node = [];
+// const nodeIDs = [];
+// const link = [];
 
 const App = () => {
+  const [nodes, setNodes] = useState([]);
+  const [links, setLinks] = useState([]);
+  const nodeIDs = [];
+  useEffect(() => {
+    // TODO: 何もしないので別の関数に切り出したい
+  }, [nodes, links]);
+
+  chrome.history.onVisited.addListener((re) => {
+    if (!nodeIDs.includes(re.id)) {
+      nodes.push({ data: { id: re.id, url: re.url } });
+      setNodes([...nodes]);
+      nodeIDs.push(re.id);
+    }
+
+    if (nodeIDs.length !== 1) {
+      links.push({
+        data: { target: re.id, source: nodeIDs[nodeIDs.length - 2] },
+      });
+      setLinks([...links]);
+    }
+    // console.log(node);
+    // console.log(link);
+  });
+
   return (
     <div>
       <h1>My new React App</h1>
-      <HistoryChart {...data} />
+      <HistoryChart {...{ nodes, links }} />
     </div>
   );
 };
