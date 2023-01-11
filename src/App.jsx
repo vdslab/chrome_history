4f9345391fbe4c5d3b744b207e9a603fba514675
itochs <halglobe0108@gmail.com>
2023-01-12 06:01:07 +0900

    backgroundとの通信

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ const App = () => {
   const [nodes, setNodes] = useState([]);
   const [links, setLinks] = useState([]);
 
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message === "ready-post-data") {
+      sendResponse("ok");
+      chrome.runtime.sendMessage("get-data", (response) => {
+        console.log("received data", response);
+        const { nodes, links } = response;
+        setNodes(nodes);
+        setLinks(links);
+      });
+    } else {
+      sendResponse("not get");
+    }
+  });
+
   return (
     <div>
       <h1>My new React App</h1>
