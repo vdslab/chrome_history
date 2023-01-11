6399e1694b78ada1f3d7aaa5dd4670d76398d2ac
itochs <halglobe0108@gmail.com>
2023-01-12 06:00:35 +0900

    backgroundに移動したため削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import HistoryChart from "./HistoryChart";
 
 const App = () => {
   const [nodes, setNodes] = useState([]);
   const [links, setLinks] = useState([]);
-  const [nodeIDs, setNodeIds] = useState([]);
-  useEffect(() => {
-    // TODO: 何もしないので別の関数に切り出したい
-  }, [nodeIDs]);
-
-  chrome.history.onVisited.addListener((re) => {
-    if (!nodeIDs.includes(re.id)) {
-      nodes.push({ data: { id: re.id, url: re.url } });
-      setNodes([...nodes]);
-      nodeIDs.push(re.id);
-      setNodeIds([...nodeIDs]);
-    }
-
-    if (nodeIDs.length !== 1) {
-      links.push({
-        data: { target: re.id, source: nodeIDs[nodeIDs.length - 2] },
-      });
-      setLinks([...links]);
-    }
-  });
 
   return (
     <div>
