f27c538812e2c8813c79d0369f10bbcb250495ff
田子友唯 <yui@tagotomotadanoMacBook-Pro.local>
2023-01-12 16:46:26 +0900

    back判定追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,6 @@
 import React, { useState } from "react";
 import HistoryChart from "./HistoryChart";
 
-<<<<<<< HEAD
-const node = [];
-const nodeIDs = [];
-const link = [];
-
-chrome.history.onVisited.addListener((re) => {
-  if (!nodeIDs.includes(re.id)) {
-    node.push({ data: { id: re.id, url: re.url } });
-    nodeIDs.push(re.id);
-  }
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message === "ready-post-data") {
-    sendResponse("ok");
-    chrome.runtime.sendMessage("get-data", (response) => {
-      // console.log("received data", response);
-      const { nodes, links } = response;
-      setNodes(nodes);
-      setLinks(links);
-    });
-  } else {
-    sendResponse("not get");
-  }
-  var referrer = document.referrer;
-  // console.log("ref", referrer);
-});
-=======
 const App = () => {
   const [nodes, setNodes] = useState([]);
   const [links, setLinks] = useState([]);
@@ -48,7 +20,6 @@ const App = () => {
     var referrer = document.referrer;
     console.log("ref", referrer);
   });
->>>>>>> 03e12d218f0bea9eba77447074d557be9d625814
 
   return (
     <div>
