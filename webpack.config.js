340ca53d614e3787597e7f92357cc8f46fd19836
itochs <halglobe0108@gmail.com>
2023-01-23 16:14:52 +0900

    別ページのentry設定

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
     background: path.join(__dirname, "src/background.js"),
     main: path.join(__dirname, "src/index.js"),
+    pages: path.join(__dirname, "src/HistoryPage/main-history.js")
   },
   output: {
     path: path.join(__dirname, "dist/js"),
