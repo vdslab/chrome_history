dcd8988b6b387916283bf2c0cffb05f58de10916
tagoyui <yui@tagotomotadanoMacBook-Pro.local>
2023-01-24 16:15:18 +0900

    try getVisits

--- src/HistoryPage/HistoryPage.jsx
+++ src/HistoryPage/HistoryPage.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 export default function HistoryPage() {
+  chrome.history.search({ text: "" }, (item) => {
+    console.log(item[90].url);
+    item.map((value) => {
+      chrome.history.getVisits({ url: value.url }, (VisitItem) => {
+        console.log(VisitItem);
+      });
+    });
+  });
   return (
     <>
       <div>
