016e849559f40fc76f72b2120115571cc9acbdb5
itochs <halglobe0108@gmail.com>
2023-01-25 20:21:42 +0900

    get family

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import HistoryChart from "./HistoryChart";
 const App = () => {
   const [nodes, setNodes] = useState([]);
   const [links, setLinks] = useState([]);
+  const [family, setFamily] = useState([]);
 
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message === "ready-post-data") {
       sendResponse("ok");
       chrome.runtime.sendMessage("get-data", (response) => {
         // console.log("received data", response);
-        const { nodes, links } = response;
+        const { nodes, links, family } = response;
         setNodes(nodes);
         setLinks(links);
+        setFamily(family);
       });
     } else {
       sendResponse("not get");
