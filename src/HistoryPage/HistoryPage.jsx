072f31f12c684bf7feabe59f3ad0d881307f29e9
itochs <halglobe0108@gmail.com>
2023-01-30 02:37:44 +0900

    copy popup js

--- src/HistoryPage/HistoryPage.jsx
+++ src/HistoryPage/HistoryPage.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import HistoryChart from "../HistoryChart";
 
 export default function HistoryPage() {
+  const [nodes, setNodes] = useState([]);
+  const [links, setLinks] = useState([]);
+  const [family, setFamily] = useState([]);
+
+  useEffect(() => {
+    chrome.runtime.sendMessage("get-data", (response) => {
+      const { nodes, links } = response;
+      setNodes(nodes);
+      setLinks(links);
+      setFamily(family);
+      return true;
+    });
+  }, []);
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message === "ready-post-data") {
+      sendResponse("ok");
+      chrome.runtime.sendMessage("get-data", (response) => {
+        // console.log("received data", response);
+        const { nodes, links, family } = response;
+        setNodes(nodes);
+        setLinks(links);
+        setFamily(family);
+      });
+    } else {
+      sendResponse("not get");
+    }
+    var referrer = document.referrer;
+    // console.log("ref", referrer);
+  });
+
   return (
-    <>
-      <div>
-        <p>atnother page</p>
-      </div>
-    </>
+    <div>
+      <h1>My new React App</h1>
+      <HistoryChart {...{ nodes, links, family }} />
+      <button
+        onClick={() => {
+          chrome.tabs.create({
+            url: "history-page.html",
+          });
+        }}
+      >
+        click me
+      </button>
+    </div>
   );
 }
