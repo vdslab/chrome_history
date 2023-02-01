0bfc9fc7464e7bb98edfcc8218ceb317f3ab115d
itochs <halglobe0108@gmail.com>
2023-02-01 12:23:57 +0900

    conflict

--- src/HistoryPage/VisitsHIstoryChart.jsx
+++ src/HistoryPage/VisitsHIstoryChart.jsx
@@ -7,12 +7,22 @@ export default function VisitsHistoryChart() {
   const [history, setHistory] = useState([]);
   const [visits, setVisits] = useState([]);
 
+  function getLimit(filter) {
+    const startTime =
+      filter.length >= 6
+        ? new Date(filter).getTime() - 24 * 60 * 60 * 1000
+        : new Date().getTime() - Number(filter) * 60 * 60 * 1000;
+    const endTime =
+      filter.length >= 4 ? new Date(filter).getTime() : new Date().getTime();
+    return { startTime, endTime };
+  }
+
   useEffect(() => {
-    const limitTime = new Date().getTime() - 24 * 60 * 60 * 1000;
     const options = {
       text: "",
-      maxResults: 100,
-      startTime: limitTime,
+      maxResults: 100000,
+      endTime,
+      startTime,
     };
 
     (async () => {
@@ -21,181 +31,199 @@ export default function VisitsHistoryChart() {
         setHistory(historys);
       });
     })();
-  }, []);
+  }, [filter]);
 
-  if (visits.length == 0) {
+  const { startTime, endTime } = getLimit(filter);
+
+  const { nodes, links, family } = useMemo(() => {
+    if (!visits || visits.length == 0) {
+      return { nodes: null, links: null, family: null };
+    }
+
+    const { startTime } = getLimit(filter);
+
+    const uniqueVisits = visits.flat().reduce((current, now) => {
+      const { id, referringVisitId, transition, visitId, visitTime } = now;
+      if (startTime > visitTime) {
+        return current;
+      }
+      // console.log(current, now);
+      const idx = current.findIndex(({ id, referringVisitId }) => {
+        return id === now.id && referringVisitId === now.referringVisitId;
+      });
+
+      if (idx < 0) {
+        current.push({
+          id,
+          referringVisitId,
+          transitions: [transition],
+          visitIds: [visitId],
+          visitTimes: [visitTime],
+        });
+        return current;
+      }
+
+      current[idx].transitions.push(transition);
+      current[idx].visitIds.push(visitId);
+      current[idx].visitTimes.push(visitTime);
+
+      return current;
+    }, []);
+
+    const uniqueIdVisits = Array.from(
+      new Map(
+        uniqueVisits.map((item) => {
+          const key = item.id;
+          return [key, item];
+        })
+      ).values()
+    ).map(({ data: { id } }) => id);
+
+    const nodes = uniqueIdVisits.map((visit) => {
+      const hist = history.find((h) => h.id === visit.id);
+      return {
+        data: {
+          id: visit.id,
+          title: hist.title,
+          url: hist.url,
+          time: hist.visitTime,
+        },
+      };
+    });
+
+    const raw_links = uniqueVisits
+      .map((visit) => {
+        const source = uniqueVisits.find((from) => {
+          return from.visitIds.some((id) => id === visit.referringVisitId);
+        });
+
+        if (!source) {
+          return;
+        }
+
+        return {
+          data: {
+            target: visit.id,
+            source: source.id,
+          },
+        };
+      })
+      .filter((item) => item);
+
+    const uniqueRawLinks = Array.from(
+      new Map(
+        raw_links.map((item) => {
+          const key = item.data.target + item.data.source;
+          return [key, item];
+        })
+      ).values()
+    );
+
+    const raw_family = [];
+    const links = [];
+    uniqueRawLinks.forEach(({ data: { target, source } }) => {
+      // link
+      const alreadyParent =
+        raw_family.some(({ data: { parent } }) => target === parent) &&
+        links.length != 1;
+
+      const isChild2Child = raw_family.some(({ data: { children } }) => {
+        return children.some((id) => id === target);
+      });
+
+      const isBack = alreadyParent || isChild2Child;
+      links.push({
+        data: {
+          target,
+          source,
+          isBack,
+        },
+      });
+      // family
+      const fidx = raw_family.findIndex(({ parent }) => parent === source);
+
+      if (fidx < 0) {
+        if (alreadyParent) {
+          return;
+        }
+
+        raw_family.push({
+          data: {
+            parent: source,
+            children: [target],
+          },
+        });
+        return;
+      }
+
+      const newChild = [...new Set([...family[fidx].data.children, target])];
+      raw_family[fidx].data.children = newChild;
+    });
+
+    return { nodes, links, family: raw_family };
+  });
+
+  if (new Date(filter).getTime() > new Date().getTime()) {
+    return (
+      <div>
+        <p>no data</p>
+      </div>
+    );
+  }
+
+  if (filter <= 0) {
+    return (
+      <div>
+        <p>no data</p>
+      </div>
+    );
+  }
+
+  // const findsource = (referringId) => {
+  //   if (referringId === 0) {
+  //     return;
+  //   } else {
+  //     for (const v of visits) {
+  //       for (const w of v) {
+  //         if (referringId === Number(w.visitId)) {
+  //           return { id: w.id, time: w.visitTime };
+  //         }
+  //       }
+  //     }
+  //   }
+  // };
+
+  // const reverseFamily = visits.map((value) => {
+  //   return {
+  //     target: value[0].id,
+  //     source: value
+  //       .map((v) => {
+  //         const sourceid = findsource(Number(v.referringVisitId));
+  //         if (sourceid) {
+  //           return sourceid.id;
+  //         }
+  //       })
+  //       .filter((element) => element),
+  //     time: value
+  //       .map((w) => {
+  //         const sourcetime = findsource(Number(w.referringVisitId));
+  //         if (sourcetime) {
+  //           return sourcetime.time;
+  //         }
+  //       })
+  //       .filter((element) => element),
+  //   };
+  // });
+
+  // // var referrer = document.referrer;
+
+  if (!nodes || !links || !family) {
     return (
       <div>
         <p>getting your visits...</p>
       </div>
     );
   }
-
-  const uniqueVisits = Array.from(
-    new Map(
-      visits.flat().map((item) => {
-        const key = item.id + item.referringVisitId;
-        return [key, item];
-      })
-    ).values()
-  ).sort((l, r) => {
-    return l.visitTime - r.visitTime;
-  });
-  const linkVisits = uniqueVisits.filter((item) => item.transition === "link");
-
-  const uniqueIdVisits = Array.from(
-    new Map(
-      linkVisits.map((item) => {
-        const key = item.id;
-        return [key, item];
-      })
-    ).values()
-  );
-
-  const nodes = uniqueIdVisits.map((visit) => {
-    const hist = history.find((h) => h.id === visit.id);
-    return {
-      data: {
-        id: visit.id,
-        title: hist.title,
-        url: hist.url,
-      },
-    };
-  });
-
-  const raw_links = linkVisits
-    .map((visit) => {
-      const source = linkVisits.find((from) => {
-        return visit.referringVisitId === from.visitId;
-      });
-      if (!source) {
-        return;
-      }
-
-      return {
-        data: {
-          target: visit.id,
-          source: source.id,
-        },
-      };
-    })
-    .filter((item) => item);
-
-  //   const node = visits.map((value) => {
-  //     const historyItem = history.find((h) => h.id === value[0].id);
-  //     return {
-  //       data: {
-  //         id: value[0].id,
-  //         index: 0,
-  //         title: historyItem.title,
-  //         url: historyItem.url,
-  //       },
-  //     };
-  //   });
-
-  const findsource = (referringId) => {
-    if (referringId === 0) {
-      return;
-    } else {
-      for (const v of visits) {
-        for (const w of v) {
-          if (referringId === Number(w.visitId)) {
-            return { id: w.id, time: w.visitTime };
-          }
-        }
-      }
-    }
-  };
-
-  const reverseFamily = visits.map((value) => {
-    return {
-      target: value[0].id,
-      source: value
-        .map((v) => {
-          const sourceid = findsource(Number(v.referringVisitId));
-          if (sourceid) {
-            return sourceid.id;
-          }
-        })
-        .filter((element) => element),
-      time: value
-        .map((w) => {
-          const sourcetime = findsource(Number(w.referringVisitId));
-          if (sourcetime) {
-            return sourcetime.time;
-          }
-        })
-        .filter((element) => element),
-    };
-  });
-
-  //   const edges = reverseFamily
-  //     .map((value) => {
-  //       const data = value;
-  //       return value.source.map((element, index) => {
-  //         return {
-  //           data: {
-  //             target: data.target,
-  //             source: element,
-  //             time: data.time[index],
-  //             isBack: true,
-  //           },
-  //         };
-  //       });
-  //     })
-  //     .filter((element) => element.length)
-  //     .flat();
-  // var referrer = document.referrer;
-
-  const uniqueRawLinks = Array.from(
-    new Map(
-      raw_links.map((item) => {
-        const key = item.data.target + item.data.source;
-        return [key, item];
-      })
-    ).values()
-  );
-
-  const raw_family = [];
-  const links = [];
-  uniqueRawLinks.forEach(({ data: { target, source } }) => {
-    // link
-    const alreadyParent =
-      raw_family.some(({ data: { parent } }) => target === parent) &&
-      links.length != 1;
-
-    const isChild2Child = raw_family.some(({ data: { children } }) => {
-      return children.some((id) => id === target);
-    });
-
-    const isBack = alreadyParent || isChild2Child;
-    links.push({
-      data: {
-        target,
-        source,
-        isBack,
-      },
-    });
-    // family
-    const fidx = raw_family.findIndex(({ parent }) => parent === source);
-
-    if (fidx < 0) {
-      if (alreadyParent) {
-        return;
-      }
-
-      raw_family.push({
-        data: {
-          parent: source,
-          children: [target],
-        },
-      });
-      return;
-    }
-
-    const newChild = [...new Set([...family[fidx].data.children, target])];
-    raw_family[fidx].data.children = newChild;
-  });
 
   return (
     <>
