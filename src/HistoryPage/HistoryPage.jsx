e204dc8b6610be11319d111d53dae60e4cf22eff
itochs <halglobe0108@gmail.com>
2023-02-12 21:33:40 +0900

    routingの修正と見た目の修正

--- src/HistoryPage/HistoryPage.jsx
+++ src/HistoryPage/HistoryPage.jsx
@@ -2,7 +2,14 @@ import React, { useEffect, useState } from "react";
 import HotHistoryChart from "./HotHistoryChart";
 import VisitsHistoryChart from "./VisitsHIstoryChart";
 import "bulma/css/bulma.css";
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  Route,
+  Link,
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
 import { ErrorBoundary } from "./ErrorBound";
 
 function Header() {
@@ -17,34 +24,23 @@ function Header() {
   );
 }
 
-function Router() {
+function Navigation() {
   return (
-    <HashRouter>
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-2">
-              <aside className="menu">
-                <ul className="menu-list">
-                  <li>
-                    <Link to="/HotHistory">今の履歴</Link>
-                  </li>
-                  <li>
-                    <Link to="/VisitsHistory">過去の履歴</Link>
-                  </li>
-                </ul>
-              </aside>
-            </div>
-            <div className="column is-10">
-              <Routes>
-                <Route path="/HotHistory" element={<HotHistory />}></Route>
-                <Route path="/VisitsHistory" element={<VisitsHistory />} />
-              </Routes>
-            </div>
-          </div>
-        </div>
-      </section>
-    </HashRouter>
+    <div className="navbar">
+      <aside className="navbar-menu">
+        <ul className="navbar-start">
+          <li className="navbar-item">
+            <Link to={"/index.html"}>Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="HotHistory">今の履歴</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="VisitsHistory">過去の履歴</Link>
+          </li>
+        </ul>
+      </aside>
+    </div>
   );
 }
 
@@ -190,13 +186,72 @@ function VisitsHistory() {
   );
 }
 
+function Home() {
+  const colSize = 6;
+  const colMax = 12;
+  const colPos = `is-${colSize} is-offset-${Math.floor(
+    (colMax - colSize) / 2
+  )}`;
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="block has-text-centered">
+          <h2 className="title has-text-link">History Tree</h2>
+          <p className="subtitle">自分の履歴をグラフで見る</p>
+        </div>
+
+        <div className="columns">
+          <div className={`column ${colPos}`}>
+            <article className="message is-dark">
+              <div className="message-header">
+                <p>使い方</p>
+              </div>
+              <div className="message-body content">
+                <h4>今の履歴</h4>
+                <p>chromeのウィンドウを開いてからの履歴が描画されます</p>
+                <h4>過去の履歴</h4>
+                <p>
+                  どの期間の履歴を描画するか選択して，履歴を描画することができます
+                </p>
+                <p>選択できる期間は以下の通りです</p>
+                <ul>
+                  <li>現在から6時間前まで</li>
+                  <li>現在から12時間前まで</li>
+                  <li>現在から24時間前まで</li>
+                  <li>特定の日付における，0時から24時までの24時間</li>
+                </ul>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route
+        element={
+          <div>
+            <Header />
+            <Navigation />
+            <Outlet />
+          </div>
+        }
+        path="/index.html"
+      >
+        <Route index element={<Home />}></Route>
+        <Route element={<HotHistory />} path="HotHistory"></Route>
+        <Route element={<VisitsHistory />} path="VisitsHistory"></Route>
+      </Route>
+    </>
+  )
+);
+
 export default function HistoryPage() {
   // var referrer = document.referrer;
 
-  return (
-    <div>
-      <Header />
-      <Router />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
